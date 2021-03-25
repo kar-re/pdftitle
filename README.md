b00954c3b03a3cbd711e5ee0f25e51aac42f5c2f
kar-re <kaspianjakobsson@gmail.com>
2021-03-25 16:42:08 +0100

    Changed name because of title mistake

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# PdfTitle
+# EditPdf
 
-**PdfTitle** is a small python application for changing title metadata of PDFs.
+**EditPdf** is a small python application for changing title metadata of PDFs.
 
 ## Usage
 ```
-pdftitle inputfilename outputpath
+editpdf inputfilename outputpath
 ```
 `inputfilename` can be a specific file, or *.pdf, or similar. It can include whole directories of pdfs as well.
 
 `outputpath` points to the folder where the changed files should be stored.
 
+:
--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="pdftitle",
-    version='0.1',
+    name="editpdf",
+    version='0.1.1',
     description='PDF File title editing CLI application',
     author='Kaspian Jakobsson',
     author_email='kaspian.jakobsson@gmail.com',
     url='https://github.com/kar-re/pdftitle',
     license='MIT',
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      
+    python_requires='>=3.6',                
     py_modules=['metachange'],
     install_requires=[
         'Click',
@@ -15,6 +26,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        pdftitle=metachange:cli
+        editpdf=metachange:cli
     ''',
 )
